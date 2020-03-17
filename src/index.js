d6c80420b229d2fa3d4cf878a8e90a4a107d5211
Yauheni_Bulava <jonbulova97@gmail.com>
2020-03-17 18:28:22 +0300

    feat: implemented function 'reverse'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  var result = String(n).split("").reverse().join("");
+  console.log(result);
+  return Number(result);  
 }
+
+//console.log(reverse(-192));
